562482b05e4e02c7c074304143f2dc7fb45bfa85
LyuboslavT <tsvetkovski.lyuboslav@gmail.com>
2025-03-03 17:01:31 +0200

    Update 03_catalogue.py

--- Programming Fundamentals/12_objects_and_classes/02_EXERCISE/03_catalogue.py
+++ Programming Fundamentals/12_objects_and_classes/02_EXERCISE/03_catalogue.py
@@ -1,6 +1,3 @@
-from numpy.ma.core import append, product
-
-
 class Catalogue:
 
     def __init__(self, name: str):
@@ -9,7 +6,23 @@ class Catalogue:
 
     def add_product(self, name: str):
 
-        self.products.append(product)
+        self.products.append(name)
 
     def get_by_letter(self, first_letter: str):
-        
+         return [name for name in self.products if name.startswith(first_letter)]
+
+    def __repr__(self):
+
+        sorted_products = sorted(self.products)
+        return f"Items in the {self.name} catalogue:\n" + '\n'.join(sorted_products)
+
+
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
