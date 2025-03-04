5251cebfa3caebd97f8b79f49855d5cba362fa0c
LyuboslavT <tsvetkovski.lyuboslav@gmail.com>
2025-03-04 13:33:53 +0200

    Update 05_class.py

--- Programming Fundamentals/12_objects_and_classes/02_EXERCISE/05_class.py
+++ Programming Fundamentals/12_objects_and_classes/02_EXERCISE/05_class.py
@@ -1,6 +1,3 @@
-from numpy.ma.extras import average
-
-
 class Class:
     __students_counter = 22
 
@@ -13,17 +10,21 @@ class Class:
 
     def add_student(self, name: str, grade: float):
 
-        self.students.append(name)
-        self.grades.append(grade)
-        Class.__students_counter -= 1
+        if Class.__students_counter > 0:
+            self.students.append(name)
+            self.grades.append(grade)
+            Class.__students_counter -= 1
 
     def get_average_grade(self):
-        average_grade = sum(self.grades) / len(self.grades)
+
+        average_grade = float(sum(self.grades) / len(self.grades))
+        return average_grade
+
+
 
     def __repr__(self):
-
-        return f"The students in {self.name}: {", ".join(self.students)}.\n"
-
+        return f"The students in {self.name}: {', '.join(self.students)}.\n" \
+               f"Average Grade: {self.get_average_grade():.2f}"
 
 a_class = Class("11B")
 a_class.add_student("Peter", 4.80)
